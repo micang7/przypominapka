161f3261f83336cad83f9d980dec95e0a2d632ad
micang7 <mckinley@interia.pl>
2026-05-10 15:32:46 +0200

    feat(server): config drizzle orm

---
+++ server/drizzle.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'drizzle-kit';
+import { env } from './src/env.js';
+
+export default defineConfig({
+  schema: './src/db/schema.ts',
+  out: './src/db/migrations',
+  dialect: 'postgresql',
+  dbCredentials: {
+    host: env.POSTGRES_HOST,
+    port: env.POSTGRES_PORT,
+    user: env.POSTGRES_USER,
+    password: env.POSTGRES_PASSWORD,
+    database: env.POSTGRES_DB,
+  },
+});
--- server/src/env.ts
+++ server/src/env.ts
@@ -6,6 +6,11 @@ const envSchema = z.object({
     .enum(['development', 'production', 'test'])
     .default('development'),
   PORT: z.coerce.number().default(3000),
+  POSTGRES_HOST: z.string(),
+  POSTGRES_PORT: z.coerce.number(),
+  POSTGRES_DB: z.string(),
+  POSTGRES_USER: z.string(),
+  POSTGRES_PASSWORD: z.string(),
 });
 
 export const env = envSchema.parse(process.env);
