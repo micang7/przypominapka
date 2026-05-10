f09aebc831a9f8dd8228e97699e9b1ca03d2c5f5
micang7 <mckinley@interia.pl>
2026-05-10 12:58:07 +0200

    chore(server): setup initial express server

---
+++ server/src/env.ts
@@ -0,0 +1,13 @@
+import 'dotenv/config';
+import { z } from 'zod';
+
+const envSchema = z.object({
+  NODE_ENV: z
+    .enum(['development', 'production', 'test'])
+    .default('development'),
+  PORT: z.coerce.number().default(3000),
+});
+
+export const env = envSchema.parse(process.env);
+
+export type Env = z.infer<typeof envSchema>;
